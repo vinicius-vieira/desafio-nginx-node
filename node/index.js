9bc2155e64036b06660e0a0755734237edc1f68a
Vinicius Vieira <douglas.vinicius@gmail.com>
2023-09-07 20:14:19 -0300

    Fix Desafio DB

--- node/index.js
+++ node/index.js
@@ -1,9 +1,47 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const createTale = `CREATE TABLE people(id int not null auto_increment, name varchar(256),  primary key(id) )`;
+connection.query(createTale);
+
+const query = `INSERT INTO people(name) VALUES('Vinicius')`;
+connection.query(query);
+connection.end();
 
 app.get('/', (req,res) =>{
-    res.send('<h1>Full Cycle: Vinicius</h1>')
+
+    let body = '<h1>Full Cycle!!</h1>';
+    body += '<h3>Lista de pessoas</h3>';
+    body += '<ul>';
+
+    let con = mysql.createConnection(config);
+
+    con.query( `SELECT name FROM people`, (err, result, fields) => {
+                if (err) throw err;
+
+                result.forEach(people => {
+                    body += '<li>';
+                    body += people.name;
+                    body += '</li>';
+                });
+            });
+    
+    con.end();
+
+    body += '</ul>';
+
+    res.send( body );
 })
 
 app.listen(port,() =>{
